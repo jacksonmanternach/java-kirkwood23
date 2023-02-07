886654834ca1287a8d32f8b0afaafa30f67a6d96
Jackson Manternach <jacksonmanternach@gmail.com>
2023-02-07 15:29:39 -0600

    completed codegym lv 1

---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0109/Solution.java
@@ -0,0 +1,12 @@
+package com.codegym.task.task01.task0109;
+
+/* 
+We solemnly publish and declare these variables...
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 7; int b = 9;
+    }
+}
